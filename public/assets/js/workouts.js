c62207d6513882a4cdb05b82797d8918f72153d8
Goldpm <lowedavidadam@gmail.com>
2019-12-11 23:51:58 -0500

    added selectors and post call

--- public/assets/js/workouts.js
+++ public/assets/js/workouts.js
@@ -1,6 +1,8 @@
 const table = $('#workTable');
 const modalBtn = $('#modalBtn');
 const submit = $('#submit');
+const newFit = $('#newFit')
+const workoutReps = $('#workoutReps')
 
 getResults();
 
@@ -8,7 +10,6 @@ table.append(`
 <tr>
 <td>do</td>
 <td>re</td>
-<td>me</td>
 </tr>
 `);
 
@@ -21,60 +22,33 @@ function getResults() {
               return;
           }
           response.json().then(function(data) {
-              console.log(data)
+              console.log(data);
           });
       })
       .catch(function(err) {
           console.log("Fetch Error :-S", err);
       });
 }
-
-submit.on(events, function () {
-  
-});
-
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
-    var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
-
-    var newSleepState = {
-      sleepy: newSleep
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/workouts/" + id, {
-      type: "PUT",
-      data: newSleepState
-    }).then(
-      function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-
-    var newWorkout = {
-      name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
-    };
-
-    // Send the POST request.
-    $.ajax("/api/workouts", {
-      type: "POST",
-      data: newWorkout
-    }).then(
-      function() {
-        console.log("created new workout");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+  submit.click(function (e) {
+    e.preventDefault();
+    const fit = newFit.val();
+    const reps = workoutReps.val();
+    fetch("/api/workouts", {
+      method: "post",
+      headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          fit,
+          reps
+      })
+  })
+  .then(function(response) {
+    console.log(response)
+      return response.json();
+  })
   });
 });

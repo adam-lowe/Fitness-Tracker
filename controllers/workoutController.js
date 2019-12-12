a4d8cb0521c03b319c8390ca82fd59537640d60b
Goldpm <lowedavidadam@gmail.com>
2019-12-11 23:31:07 -0500

    added fetch route

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,20 +1,42 @@
 var express = require("express");
+const databaseUrl = "workoutList";
+const collections = ["workouts"];
+const mongoose = require("mongoose");
+const mongojs = require("mongojs");
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
 
 var router = express.Router();
 
 // Import the model (workout.js) to use its database functions.
-var Workout = require("../models/workouts.js/index.js");
 
 // Create all our routes and set up logic within those routes where required.
 
+router.get("/api/workouts", function(req, res) {
+  console.log(req.body);
+
+  db.workouts.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
 router.post("/api/workouts", function(req, res) {
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+  console.log(req.body);
+
+  db.workouts.insert(req.body, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 router.put("/api/workouts/:id", function(req, res) {
